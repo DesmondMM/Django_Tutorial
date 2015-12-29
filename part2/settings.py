ad336255b717e2b8c46f89aece33dcabf8042320
DesmondMM <mutsakaradesmond@gmail.com>
2015-12-29 09:29:17 +0200

    Completed Section 7  (Customize Admin Page)

--- part2/settings.py
+++ part2/settings.py
@@ -55,8 +55,9 @@ ROOT_URLCONF = 'part2.urls'
 
 TEMPLATES = [
     {
+
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- polls/models.py
+++ polls/models.py
@@ -6,8 +6,10 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question_text
+
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
@@ -20,6 +22,12 @@ class Question(models.Model):
         time = timezone.now() - datetime.timedelta(days=30)
         old_question = Question(pub_date=time)
         self.assertEqual(old_question.was_published_recently(), False)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        was_published_recently.admin_order_field = 'pub_date'
+        was_published_recently.boolean = True
+        was_published_recently.short_description = 'Published recently?'
 
 def test_was_published_recently_with_recent_question(self):
          """
