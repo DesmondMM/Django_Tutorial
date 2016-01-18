128eb0df60e0135327e88223304bc6ce69b7e76c
DesmondMM <mutsakaradesmond@gmail.com>
2016-01-18 08:24:32 +0200

    Added 900sec expiry value and 30 days refresh token value to the jwt token

--- part2/settings.py
+++ part2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,4 +143,8 @@ REST_FRAMEWORK = {
     )
 }
 
-
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=900),
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=30),
+}
